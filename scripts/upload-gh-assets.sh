ce8628fe9145f77dadbd0ec537a2c243205fac68
Doug Schaapveld <djschaap@users.noreply.github.com>
2023-11-16 11:21:24 -0600

    - scripts/upload-gh-assets.sh: allow for dist suffix in RPM release

--- scripts/upload-gh-assets.sh
+++ scripts/upload-gh-assets.sh
@@ -9,12 +9,12 @@ T=$(mktemp -d)
 
 # Upload artifacts with static/fixed names for ease of consumption.
 for F in extract/RPMS/x86_64/*.rpm ; do
-  n=$(echo $F | sed -e "s,.*/\([0-9A-Za-z-]*\)-${RPM_VERSION}-${RPM_RELEASE}\(.x86_64.rpm\),\1\2,")
+  n=$(echo $F | sed -e "s,.*/\([0-9A-Za-z-]*\)-${RPM_VERSION}-${RPM_RELEASE}\(.[0-9A-Za-z]*.x86_64.rpm\),\1\2,")
   echo "include ${F} as ${n}"
   cp $F "${T}/${n}"
 done
 for F in extract/SRPMS/*.rpm ; do
-  n=$(echo $F | sed -e "s,.*/\([0-9A-Za-z-]*\)-${RPM_VERSION}-${RPM_RELEASE}\(.src.rpm\),\1\2,")
+  n=$(echo $F | sed -e "s,.*/\([0-9A-Za-z-]*\)-${RPM_VERSION}-${RPM_RELEASE}\(.[0-9A-Za-z]*.src.rpm\),\1\2,")
   echo "include ${F} as ${n}"
   cp $F "${T}/${n}"
 done
